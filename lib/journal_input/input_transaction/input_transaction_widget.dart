8a5c94134b606ace4164e2ee376dcb4036529739
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-16 09:12:21 +0000

    Updating to latest FlutterFlow output.

--- lib/journal_input/input_transaction/input_transaction_widget.dart
+++ lib/journal_input/input_transaction/input_transaction_widget.dart
@@ -500,6 +500,30 @@ class _InputTransactionWidgetState extends State<InputTransactionWidget> {
                             ),
                           ),
                         ),
+                        Text(
+                          valueOrDefault<String>(
+                            _model.accountTag,
+                            'error',
+                          ),
+                          style:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    font: GoogleFonts.notoSansJp(
+                                      fontWeight: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .fontWeight,
+                                      fontStyle: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .fontStyle,
+                                    ),
+                                    letterSpacing: 0.0,
+                                    fontWeight: FlutterFlowTheme.of(context)
+                                        .bodyMedium
+                                        .fontWeight,
+                                    fontStyle: FlutterFlowTheme.of(context)
+                                        .bodyMedium
+                                        .fontStyle,
+                                  ),
+                        ),
                       ].divide(SizedBox(height: 8.0)),
                     ),
                     Column(
